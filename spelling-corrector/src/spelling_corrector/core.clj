d932983f9daa6e7d7f6926678f1182e0ff4b5a35
Colin Cross <copro315@gmail.com>
2013-07-08 15:14:26 -0500

    Add testing functions

--- spelling-corrector/src/spelling_corrector/core.clj
+++ spelling-corrector/src/spelling_corrector/core.clj
@@ -1,6 +1,14 @@
-(ns spelling-corrector.core)
+(ns spelling-corrector.core
+  (:require [spelling-corrector.parsers :as parsers]
+            [spelling-corrector.spell :as spell]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def corpus (parsers/read-sample-file "/Users/ccross/dev/spelling-jam/data/big.txt"))
+
+(defn validate-spellings-for-word [word-map]
+  (let [word (first (keys word-map))
+        incorrect-spellings (get word-map word)]
+    (every? #(= word (spell/correct corpus %)) incorrect-spellings)))
+
+(defn validate-with-test-file [file]
+  (let [spellings (parsers/read-test-file file)]
+    (every? validate-spellings-for-word spellings)))
