fda2c3bedf1d31ec8f6db6d9d739a13ec7d0c0ce
Colin Cross <copro315@gmail.com>
2013-07-08 15:41:03 -0500

    Change testing functions to return an accuracy rating

--- spelling-corrector/src/spelling_corrector/core.clj
+++ spelling-corrector/src/spelling_corrector/core.clj
@@ -4,11 +4,20 @@
 
 (def corpus (parsers/read-sample-file "/Users/ccross/dev/spelling-jam/data/big.txt"))
 
-(defn validate-spellings-for-word [word-map]
+(defn accuracy-report-for-word [word-map]
   (let [word (first (keys word-map))
-        incorrect-spellings (get word-map word)]
-    (every? #(= word (spell/correct corpus %)) incorrect-spellings)))
+        incorrect-spellings (get word-map word)
+        hits (count (filter #(= word (spell/correct corpus %)) incorrect-spellings))]
+    {:misses (- (count incorrect-spellings) hits) :total (count incorrect-spellings)}))
 
-(defn validate-with-test-file [file]
+(defn accuracy-for-test-file [file]
   (let [spellings (parsers/read-test-file file)]
-    (every? validate-spellings-for-word spellings)))
+    (reduce (fn [result spelling]
+              (let [misses (+ (:misses result))
+                    total (+ (:total result))
+                    hits (- total misses)])
+              {:hits hits
+               :misses misses
+               :total total
+               :accuracy (/ hits accuracy)})
+            {:misses 0 :total 0} spellings)))
