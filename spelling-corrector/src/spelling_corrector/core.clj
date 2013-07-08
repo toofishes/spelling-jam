d57a9481167c9ae93d1dd441c713c068b336620e
Colin Cross <copro315@gmail.com>
2013-07-08 13:14:52 -0500

    Add lein project

---
+++ spelling-corrector/src/spelling_corrector/core.clj
@@ -0,0 +1,6 @@
+(ns spelling-corrector.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ spelling-corrector/test/spelling_corrector/core_test.clj
@@ -0,0 +1,7 @@
+(ns spelling-corrector.core-test
+  (:require [clojure.test :refer :all]
+            [spelling-corrector.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
