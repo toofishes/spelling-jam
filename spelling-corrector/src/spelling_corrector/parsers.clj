eda7c1041a5136cfc8d03449b592c36f25fc169e
Colin Cross <copro315@gmail.com>
2013-07-08 14:41:42 -0500

    Add reader for test files, fix things

--- spelling-corrector/src/spelling_corrector/parsers.clj
+++ spelling-corrector/src/spelling_corrector/parsers.clj
@@ -5,7 +5,19 @@
     (assoc count-map word (if word-count (inc word-count) 0))))
 
 (defn word-seq [text]
-  (re-seq #"[a-z]+" test-file))
+  (re-seq #"[a-z]+" text))
 
 (defn read-sample-file [file]
-  (reduce inc-word-count {} (word-seq (slurp file))))
+  (reduce inc-word-count {} (word-seq (.toLowerCase (slurp file)))))
+
+(defn associate-word-from-line [line]
+  (when-not (= "#" (first line))
+    (let [words (re-seq #"[a-z]+" line)]
+      {(first words)
+       (into #{} (rest words))})))
+
+(defn word-associations [lines]
+  (map associate-word-from-line lines))
+
+(defn read-test-file [file]
+  (into {} (word-associations (line-seq (clojure.java.io/reader file)))))
